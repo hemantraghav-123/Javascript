4cf829c663fb3d8e858f803198f740d523273f1b
hemantraghav-123 <hemant.23b0131263@abes.ac.in>
2024-12-10 13:08:51 +0530

    index

---
+++ script.js
@@ -0,0 +1,11 @@
+// function sayhello() {
+//     alert("hello world")
+// }
+//  document.write("<h1>this is my first page</h1>");
+
+
+function myFunction()
+{
+    var carname="welcome";
+    document.getElementById("demo").innnerHTML=carname;
+}
