5108e550eacf21d4106b52090d73204ca440c494
hemantraghav-123 <hemant.23b0131263@abes.ac.in>
2024-12-19 16:28:26 +0530

    script

--- script.js
+++ script.js
@@ -1,11 +1,322 @@
-// function sayhello() {
-//     alert("hello world")
+// function sayHello() {
+//     alert("Hello World !")
 // }
-//  document.write("<h1>this is my first page</h1>");
+// document.write("<h1>This is my first javascript page</h1>");
 
 
-function myFunction()
-{
-    var carname="welcome";
-    document.getElementById("demo").innnerHTML=carname;
-}
+// var pi=3.14;
+// var person="are you GOD";
+// var answer="Yes I am!";
+// document.write(pi + "<br>");
+// document.write(person + "<br>");
+// document.write(answer + "<br>");
+
+// function myFunction(){
+//     var carname = "Welcome";
+//     document.getElementById("demo").innerHTML=carname;
+// }
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+// var num = false;
+// console.log(num);
+// console.log(typeof(num));
+// var num = "Devansh";
+// console.log(num);
+// console.log(typeof(num));
+// var num = null;
+// console.log(num);
+// console.log(typeof(num));
+// var num = undefined;
+// console.log(num);
+// console.log(typeof(num));
+// var numberArray = [1,2,3];
+// var animals = new Array("cat","dog","mouse","lion");
+// var person = {
+//     name:"Barack"
+
+// }
+// function myFunction(){
+// var person = {
+//     firstname : "John",
+//     lastname: "Doe",
+//     id : 5566,
+//     getinfo:function(){
+//         return this.firstname + " " +this.lastname
+//     }
+// };
+// document.getElementById("demo").innerHTML=person.getinfo();
+
+// }
+
+// var person={
+//     name : 'Ramesh',
+//     age : 30,
+//     isMarried : true,
+//     address : {
+//         street : 'Vijay Nagar',
+//         flatNo :201
+//     }
+// };
+// console.log(person);
+
+// var person2 = new Object();
+// person2.name = 'Krishnkant',
+// person2.age = 32,
+// person2.isMarried = true;
+// person2.address={};
+// person2.address.street="nagar road";
+// person2['address']['flatNO']=33;
+// console.log(person2);
+
+// var person ={
+//     name : 'Jitendra Chauhan',
+//     age:30,
+//     isMarried:true,
+//     child : 'cheeku',
+//     getInfo : function(){
+//     },
+//     address : {
+//         street :'Vijay nagar',
+//         flatNO:201
+//     }
+// }
+
+// console.log(person.name);
+// document.write(person.name);
+// document.write("<br>")
+// document.write(person.age)
+
+// function myFunction(){
+//     var carName = "Volvo";
+//     document.getElementById('demo1').innerHTML = typeof carName +" "+ carName
+// }
+// myFunction();
+// document.getElementById('demo2').innerHTML = typeof carName
+
+// var locales = {
+//     europe : function(){
+//         var myFriend = "Monique";
+//         var france = function(){
+//         var paris = function(){
+//             console.log(myFriend);
+//         };
+//         paris();
+//         };
+//         france();
+//     } 
+// }
+
+// locales.europe();
+
+// function show(){
+//     var a = "a is a local outer variable";
+//     document.write(a + "<br>");
+//     function disk(){
+//         var b = "b is a local variable";
+//         document.write(b + "<br>");
+
+//     }
+//     disk();
+// }
+// show();
+
+// var myVar ="global";
+// function check(){
+//     var myVar = "local";
+//     document.write(myVar);
+// }
+
+// var name = 'Krishan kant';
+// function first() {
+//     var a = 'Hello!';
+//     Second();
+//     var x = a + name;
+//     console.log(x)
+// }
+
+// function Second() {
+//     var b = 'Hi!'
+//     third();
+//     var z = b + name;
+//     console.log(z)
+// }
+// function third() {
+//     var c = 'Hey!'
+//     var z = c + name;
+//     console.log(z)
+// }
+// first();
+
+// var age = 20;
+// if(age>18){
+//     document.write("<b>You are eligible to vote</b>");
+// }
+
+// var grade = 'A';
+// document.write("Entering switch block<br>");
+// switch (grade) {
+//     case 'A':
+//         document.write("Good job<br>");
+//         break;
+
+//     case 'B':
+//         document.write("Pretty Good<br>");
+//         break;
+
+//     case 'C' :
+//         document.write("Passed<br>");
+//         break;   
+//     case 'D':
+//         document.write("Not so good<br>");
+//         break;
+
+//     case 'F':
+//         document.write("Failed<br>");
+//         break;   
+//     default:
+//         document.write("Unknown grade<br>");
+//         break; 
+// }
+// document.write("Exiting switch block");
+
+// var i;
+// for(i=0;i<3;i=i+1){
+//     console.log(i)
+// }
+
+// var count;
+// document.write("Starting Loop" + "<br>");
+// for(count=0;count<10;count++){
+//     document.write("Current Count : "+ count);
+//     document.write("<br>")
+// }
+// document.write("Loop Stopped!");
+
+// for(var i = 0;i<100;i++){
+//     if(i%2==0){
+//         console.log(i + " is an even number");
+//     }
+//     else{
+//          console.log(i + " is an odd number");
+//     }
+// }
+
+// var myArray = ["A","B","C"]
+// for(var i = 0;i<myArray.length;i++){
+//     console.log("The member of myArray in index "+i+" is "+myArray[i]);
+// }
+// var count = 0;
+// document.write("Starting Loop<br>");
+// while(count<10){
+//     document.write("Current Count : " + count + "<br>")
+//     count++;
+// }
+
+// document.write("Loop Stopped!");
+
+// function myFunction(){
+//     alert("Hello world");
+// }
+// function sayHello(name,age){
+//     document.write(name + " is "+ age +" year old");
+// }
+
+// var res= mul (12,30);
+// function mul (x,y)
+// { 
+//     return x*y;
+// }
+// document.write(res);
+
+// const square = function(number){
+//     return number*number
+// };
+// const x = square (4);
+// console.log(x);
+
+
+// function myFunction(g1,g2){
+//     return g1/g2;
+// }
+// const value= myFunction(8,2);
+// console.log(value);
+
+// var add2=(a,b)=>{
+//     console.log(a+b)
+// };
+// add2(20,48);
+
+
+// var add3=(a,b)=>console.log(a+b);
+// add3(34,56);
+
+// var a=100;
+// var b=10;
+// var c=15;
+// var linebreak="<br>"
+
+// document.write("a+b+c=");
+// result=a+b+c;
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("a+b=");
+// result=a+b;
+// document.write(result);
+// document.write(linebreak);
+
+
+// var a=3;
+// var b=2;
+// var linebreak="<br>"
+
+// document.write("a/b=");
+// result=a/b;
+// document.write(result);
+// document.write(linebreak);
+
+
+// function myFunction()
+// {
+//     var x=5+5;
+//     var y="5"+5;
+//     var z="hello"+5;
+//     var demoP=document.getElementById("demo");
+//     demoP.innerHTML=x+"<br>"+y+"<br>"+z;
+// }
+
+
+// if(5==="5"){
+//     console.log("true")
+// } else{
+//         console.log("false")
+//     }
+
+// if(5=="5"){
+//     console.log("true")
+//     } else{
+//         console.log("false")
+//  }
+
+// let x=6;
+// let y=3;
+// console.log(x==10 && y>1)
+// console.log(x==6 || y==5)
+// console.log(!(x==y))
+
+// let age=prompt("Enter your age:")
+// var voteable=(age<18)? "Too young":"Old enough";
+// console.log(voteable)
+
+
+var myCollection=[
+    1,"meera mam",
+    true,{
+        name:"sanjeev",age:30
+    },
+    function(name){
+        console.logmyCollection(name);
+    },
+    ["amit","bhai","hemant"]
+]
